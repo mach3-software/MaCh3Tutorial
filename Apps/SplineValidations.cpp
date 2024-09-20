98c7ed684f72383e10eaf75ca57ab5b7322feb3e
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2024-09-20 11:05:55 +0000

    Now test flat splines

--- Apps/SplineValidations.cpp
+++ Apps/SplineValidations.cpp
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
     MACH3LOG_CRITICAL("You specified arguments, but none are needed. (Program name: ", argv[0]);
     throw MaCh3Exception(__FILE__ , __LINE__ );
   }
+  MACH3LOG_INFO("Testing Spline Monolith");
 
   std::string Input = "Inputs/SplineFile.root";
   std::vector<std::string> Dials = {"Spline_0", "Spline_1", "Spline_2", "Spline_3"};
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
 
   std::vector<RespFuncType> SplineType = {kTSpline3_red, kTSpline3_red, kTSpline3_red, kTSpline3_red};
   const unsigned int Nevents = MasterSpline.size();
-  SMonolith* Splines = new SMonolith(MasterSpline, SplineType);
+  SMonolith* Splines = new SMonolith(MasterSpline, SplineType, true);
 
   std::vector< const double* > splineParsPointer(Dials.size());
   for (unsigned int i = 0; i < Dials.size(); ++i) {
@@ -37,9 +38,27 @@ int main(int argc, char *argv[])
   for(unsigned int i = 0; i < Nevents; i++) {
     outFile << "Event " << i << " weight = " << Splines->cpu_total_weights[i] << std::endl;
   }
+
+////// Testing Pre Computed Spline
+  MACH3LOG_INFO("Testing Spline Monolith with Flattened ROOT inputs");
+
+  SMonolith* SplinesFlat = new SMonolith("SplineFile.root");
+  for (unsigned int i = 0; i < Dials.size(); ++i) {
+    splineParsPointer[i] = &Dial_Values[i];
+  }
+  SplinesFlat->setSplinePointers(splineParsPointer);
+
+  SplinesFlat->Evaluate();
+
+  //KS: If using CPU this does nothing, if on GPU need to make sure we finished copying memory from
+  SplinesFlat->SynchroniseMemTransfer();
+  for(unsigned int i = 0; i < Nevents; i++) {
+    outFile << "(Flat) Event " << i << " weight = " << SplinesFlat->cpu_total_weights[i] << std::endl;
+  }
+
   outFile.close();
   delete Splines;
-
+  delete SplinesFlat;
   bool TheSame = CompareTwoFiles("Inputs/SplineMonoOut.txt", "NewSplineMonoOut.txt");
 
   if(!TheSame) {
