5a6c164b9daadda15910db6eb0f6395f1bd08817
Kamil <45295406+KSkwarczynski@users.noreply.github.com>
2025-01-06 13:32:35 +0000

    Create data.js

---
+++ dev/bench/data.js
@@ -0,0 +1,31 @@
+Data: {
+  commit: {
+    author: {
+      email: 'skwarczynskikamil@gmail.com',
+      name: 'Kamil Skwarczynski',
+      username: 'KSkwarczynski'
+    },
+    committer: {
+      email: 'skwarczynskikamil@gmail.com',
+      name: 'Kamil Skwarczynski',
+      username: 'KSkwarczynski'
+    },
+    distinct: true,
+    id: '6e5ee49d43ad9242f7f70b07ecef91e27eecf80a',
+    message: 'fix repository name',
+    timestamp: '2025-01-06T13:13:53Z',
+    tree_id: '5da580b362a7ca385aea202355776d75109f843a',
+    url: 'https://github.com/mach3-software/MaCh3Tutorial/commit/6e5ee49d43ad9242f7f70b07ecef91e27eecf80a'
+  },
+  date: 1736169427913,
+  tool: 'catch2',
+  benches: [
+    {
+      name: 'MaCh3Fitter::DragRace',
+      value: 2.06635,
+      range: '± 88.6283',
+      unit: 'ms',
+      extra: '100 samples\n1 iterations'
+    }
+  ]
+}
