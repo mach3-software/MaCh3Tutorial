461a6fb0d980ca5a27c4a00be0235812e660c954
github-action-benchmark <github@users.noreply.github.com>
2025-01-06 13:55:44 +0000

    add Benchmark (catch2) benchmark result for 0462ee8fa0d5484d591ff2bb6f5e9d87e593f41b

---
+++ data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1736171744577,
+  "repoUrl": "https://github.com/mach3-software/MaCh3Tutorial",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "skwarczynskikamil@gmail.com",
+            "name": "Kamil Skwarczynski",
+            "username": "KSkwarczynski"
+          },
+          "committer": {
+            "email": "skwarczynskikamil@gmail.com",
+            "name": "Kamil Skwarczynski",
+            "username": "KSkwarczynski"
+          },
+          "distinct": true,
+          "id": "0462ee8fa0d5484d591ff2bb6f5e9d87e593f41b",
+          "message": "use ROOT directory",
+          "timestamp": "2025-01-06T13:52:30Z",
+          "tree_id": "0aedb586640fbb24db422b1ae1330c3e18deb9f1",
+          "url": "https://github.com/mach3-software/MaCh3Tutorial/commit/0462ee8fa0d5484d591ff2bb6f5e9d87e593f41b"
+        },
+        "date": 1736171743801,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "MaCh3Fitter::DragRace",
+            "value": 2.06544,
+            "range": "± 101.177",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
