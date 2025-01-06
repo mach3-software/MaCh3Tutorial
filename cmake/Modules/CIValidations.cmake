909b2e539d73a503d73c589c742b7964332fcc12
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2025-01-06 08:06:57 +0000

    tidy cmake build

---
+++ cmake/Modules/CIValidations.cmake
@@ -0,0 +1,26 @@
+option(MaCh3Tutorial_UNITTESTS_ENABLED "Whether to build MaCh3 Unit Tests" OFF)
+option(MaCh3Tutorial_Coverage_ENABLED "Whether to build MaCh3 Coverage " OFF)
+
+CPMAddPackage(
+  NAME NuMCMCTools
+  GIT_TAG "tags/v1.0.0"
+  GIT_SHALLOW YES
+  GITHUB_REPOSITORY NuMCMCTools/NuMCMCTools
+  DOWNLOAD_ONLY YES
+)
+
+install(DIRECTORY
+    ${CPM_PACKAGE_NuMCMCTools_SOURCE_DIR}/
+    DESTINATION ${CMAKE_BINARY_DIR}/NuMCMCTools)
+
+############################  Catch2/CTest  ####################################
+if(MaCh3Tutorial_UNITTESTS_ENABLED)
+  find_package(Catch2 QUIET)
+  if(NOT Catch2_FOUND)
+    CPMAddPackage("gh:catchorg/Catch2@3.5.2")
+    LIST(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
+  endif()
+
+  include(CTest)
+  include(Catch)
+endif()
