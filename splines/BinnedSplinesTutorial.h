f4d46bb9957400e989baa78bbdd2f6e9a4df2286
Daniel Barrow <daniel.barrow@physics.ox.ac.uk>
2025-02-20 17:51:37 +0000

    Move StripDuplicatedSplines and GetEventSplines to splineFDBase

--- splines/BinnedSplinesTutorial.h
+++ splines/BinnedSplinesTutorial.h
@@ -8,11 +8,7 @@ class BinnedSplineTutorial : public splineFDBase
 {
   public:
   BinnedSplineTutorial(covarianceXsec *xsec_cov, MaCh3Modes *Modes);
-	virtual ~BinnedSplineTutorial();
-
-	void FillSampleArray(std::string SampleName, std::vector<std::string> OscChanFileNames);
-	std::vector< std::vector<int> > StripDuplicatedModes(std::vector< std::vector<int> > InputVector) {return InputVector;};
-	std::vector< std::vector<int> > GetEventSplines(std::string SampleName, int iOscChan, int EventMode, double Var1Val, double Var2Val, double Var3Val);
-
-  MaCh3Modes* Modes;
+  virtual ~BinnedSplineTutorial();
+  
+  void FillSampleArray(std::string SampleName, std::vector<std::string> OscChanFileNames);
 };
