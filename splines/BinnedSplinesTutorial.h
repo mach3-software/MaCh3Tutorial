abbedfeb7af2a6c83503bf1101e850022153352b
Daniel Barrow <daniel.barrow@physics.ox.ac.uk>
2025-02-20 15:45:07 +0000

    Revert "Move StripDuplicatedModes to splineFDBase"
    This reverts commit 8f4112b8ce4e000338da4e5e71afaba96ffb7b3d.

--- splines/BinnedSplinesTutorial.h
+++ splines/BinnedSplinesTutorial.h
@@ -2,12 +2,17 @@
 
 //MaCh3 includes
 #include "splines/splineFDBase.h"
+#include "manager/MaCh3Modes.h"
 
 class BinnedSplineTutorial : public splineFDBase
 {
   public:
   BinnedSplineTutorial(covarianceXsec *xsec_cov, MaCh3Modes *Modes);
-  virtual ~BinnedSplineTutorial();
-  
-  void FillSampleArray(std::string SampleName, std::vector<std::string> OscChanFileNames);
+	virtual ~BinnedSplineTutorial();
+
+	void FillSampleArray(std::string SampleName, std::vector<std::string> OscChanFileNames);
+	std::vector< std::vector<int> > StripDuplicatedModes(std::vector< std::vector<int> > InputVector) {return InputVector;};
+	std::vector< std::vector<int> > GetEventSplines(std::string SampleName, int iOscChan, int EventMode, double Var1Val, double Var2Val, double Var3Val);
+
+  MaCh3Modes* Modes;
 };
