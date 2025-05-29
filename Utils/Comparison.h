ca51f2c3693ca00d383f8a842b7582fa3418bdb5
Kamil Skwarczynski <skwarczynskikamil@gmail.com>
2025-05-29 14:13:49 +0000

    initial commit

--- Utils/Comparison.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Manager/Manager.h"
-
-bool CompareTwoFiles(const std::string& File1, const std::string& File2);
--- Utils/SplineMonoUtils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// MaCh3 spline includes
-#include "Splines/SplineMonolith.h"
-
-
-std::vector< std::vector<TResponseFunction_red*> > GetMasterSpline(
-                            std::string FileName, std::vector<std::string> DialNames);
