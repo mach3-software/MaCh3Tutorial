759e959cc5f6c1a318d3005424b805f8df3c2cf9
Ewan Miller <ewan.miller@kcl.ac.uk>
2025-10-09 11:21:43 +0100

    workaround for annoying bug when installing with pip... will fix properly later

--- CIValidations/PythonTests/test_fitter_sample_pdf.py
+++ CIValidations/PythonTests/test_fitter_sample_pdf.py
@@ -1,10 +1,10 @@
 ## MaCh3 imports
 import pyMaCh3
-from pyMaCh3 import samples
-from pyMaCh3 import splines
-from pyMaCh3 import fitters
-from pyMaCh3 import manager
-from pyMaCh3 import parameters
+from pyMaCh3.pyMaCh3 import samples
+from pyMaCh3.pyMaCh3 import splines
+from pyMaCh3.pyMaCh3 import fitters
+from pyMaCh3.pyMaCh3 import manager
+from pyMaCh3.pyMaCh3 import parameters
 
 ## ROOT imports
 import ROOT
