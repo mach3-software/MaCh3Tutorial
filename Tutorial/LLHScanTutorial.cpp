43b02e9097c0025c70b4f5f348ede6fab8b8cbbc
Hank Hua <h.hua23@imperial.ac.uk>
2026-01-20 16:37:59 +0000

    Added client-side example in LLHScanTutorial.

--- Tutorial/LLHScanTutorial.cpp
+++ Tutorial/LLHScanTutorial.cpp
@@ -1,6 +1,7 @@
 // MaCh3 spline includes
 #include "Fitters/MaCh3Factory.h"
 #include "SamplesTutorial/SampleHandlerTutorial.h"
+#include "MaCh3NuDock/SampleHandlerNuDock.h"
 
 int main(int argc, char *argv[]){
   // Initialise manger responsible for config handling
@@ -14,6 +15,16 @@ int main(int argc, char *argv[]){
   auto SampleConfig = Get<std::vector<std::string>>(FitManager->raw()["General"]["TutorialSamples"], __FILE__ , __LINE__);
   auto mySamples = MaCh3SampleHandlerFactory<SampleHandlerTutorial>(SampleConfig, xsec.get());
 
+  // Initialise NuDock samples
+  auto NuDockSampleConfigs = FitManager->raw()["General"]["NuDockSamples"].as<std::vector<std::string>>();
+  bool useNuDock = GetFromManager(FitManager->raw()["General"]["UseNuDock"], false, __FILE__, __LINE__);
+  std::vector<SampleHandlerNuDock*> samplesNuDock;
+  if (useNuDock){
+    for(size_t Sample_i = 0; Sample_i < NuDockSampleConfigs.size(); Sample_i++){
+      samplesNuDock.push_back(new SampleHandlerNuDock(NuDockSampleConfigs[Sample_i].c_str(), xsec.get())); 
+    }
+  }
+
   // Create MCMC Class
   std::unique_ptr<FitterBase> MaCh3Fitter = MaCh3FitterFactory(FitManager.get());
   // Add covariance to MCM
@@ -21,10 +32,18 @@ int main(int argc, char *argv[]){
   for (size_t i = 0; i < SampleConfig.size(); ++i) {
     MaCh3Fitter->AddSampleHandler(mySamples[i]);
   }
+  if (useNuDock) {
+    for(size_t i = 0; i < NuDockSampleConfigs.size(); i++){
+      MaCh3Fitter->AddSampleHandler(samplesNuDock[i]);
+    }
+  }
   // Run LLH scan
   MaCh3Fitter->RunLLHScan();
   for (size_t i = 0; i < SampleConfig.size(); ++i) {
     delete mySamples[i];
+  }
+  for(size_t i = 0; i < NuDockSampleConfigs.size(); i++){
+    delete samplesNuDock[i];
   }
   return 0;
 }
